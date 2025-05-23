f4c86809af384955b2ba1fb972b8c43134484c44
Musraf <mussraffro45@gmail.com>
2025-05-23 22:14:23 +0530

    changes1

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,10 @@ const app = express()
 
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+ origin: [
+    "http://localhost:5173",                    
+    "https://newshuntt19.netlify.app/news"     
+  ], 
   credentials: true               
 }));
 
